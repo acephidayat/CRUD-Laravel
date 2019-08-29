f2941e24b516b236cb1f8e91e5b3a09ddf050874
unknown <acephidayat127@gmail.com>
2019-08-29 20:58:10 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+
+@extends('layout.main')
+
+@section('title', 'Web Programming Acep')
+    
+@section('container')
+    
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Hello, world!</h1>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+  
